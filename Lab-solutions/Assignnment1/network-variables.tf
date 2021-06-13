4280cd93bd712b900867bb08cf85ec635261cfac
seetha-scb <85385431+seetha-scb@users.noreply.github.com>
2021-06-13 18:25:31 +0530

    Add files via upload

---
+++ Lab-solutions/Assignnment1/network-variables.tf
@@ -0,0 +1,8 @@
+variable "network-vnet-cidr" {
+  type        = string
+  description = "The CIDR of the network VNET"
+}
+variable "network-subnet-cidr" {
+  type        = string
+  description = "The CIDR for the network subnet"
+}
---
+++ Lab-solutions/Assignnment1/terraform.tfvars
@@ -0,0 +1,6 @@
+#Azure authentication variables
+azure_subscription_id = ""
+azure_client_id = ""
+azure_client_secret = ""
+azure_tenant_id = ""
+
