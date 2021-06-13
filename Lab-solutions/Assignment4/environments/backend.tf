15388bbb3b7229df9bd62dd9675ade206ecdb137
seetha-scb <85385431+seetha-scb@users.noreply.github.com>
2021-06-13 19:06:44 +0530

    Delete backend.tf

--- Lab-solutions/Assignment4/environments/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "multitstategroup"
-    storage_account_name  = "multitstatestorage"
-    container_name        = "multitstatecontainer"
-    key                   = "terraform.tfstate"
-  }
-}
