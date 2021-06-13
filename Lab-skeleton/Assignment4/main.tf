a59528a30f045d7d7a3ce05edb3011e07b7ee852
seetha-scb <85385431+seetha-scb@users.noreply.github.com>
2021-06-13 18:06:12 +0530

    Add files via upload

---
+++ Lab-skeleton/Assignment4/main.tf
@@ -0,0 +1,14 @@
+#Create Resource Group
+resource "azurerm_resource_group" "resource_group" {
+  
+}
+ 
+#Create Storage account
+resource "azurerm_storage_account" "storage_account" {
+  
+}
+ 
+#Add index.html to blob storage
+resource "azurerm_storage_blob" "example" {
+  
+}
