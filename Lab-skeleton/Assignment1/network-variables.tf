35cdd2fb72e81a971e9aac4003ab37a1dcf62f85
seetha-scb <85385431+seetha-scb@users.noreply.github.com>
2021-06-13 17:42:06 +0530

    Delete Lab-skeleton/Assignment1 directory

--- Lab-skeleton/Assignment1/network-variables.tf
+++
@@ -1,8 +0,0 @@
-variable "network-vnet-cidr" {
-  type        = string
-  description = "The CIDR of the network VNET"
-}
-variable "network-subnet-cidr" {
-  type        = string
-  description = "The CIDR for the network subnet"
-}
