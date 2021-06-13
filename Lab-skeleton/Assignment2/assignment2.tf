d856a7d8732aa770e4789894be6640a750ae2ae5
seetha-scb <85385431+seetha-scb@users.noreply.github.com>
2021-06-13 18:00:11 +0530

    Update assignment2.tf

--- Lab-skeleton/Assignment2/assignment2.tf
+++ Lab-skeleton/Assignment2/assignment2.tf
@@ -1,5 +1,23 @@
 #Azure authentication variables
-
+variable "azure_subscription_id" {
+  type = string
+  description = "Azure Subscription ID"
+}
+variable "azure_client_id" {
+  type = string
+  description = "Azure Client ID"
+}
+variable "azure_client_secret" {
+  type = string
+  description = "Azure Client Secret"
+}
+variable "azure_tenant_id" {
+  type = string
+  description = "Azure Tenant ID"
+}
+variable "prefix" {
+  default = "tfvmex"
+}
 
 terraform {
   required_providers {
